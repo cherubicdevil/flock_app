74fbe43bbb6497a09a25c2cc4e62a24798449bbf
Kevin Gao <kevin.gaobears@berkeley.edu>
2021-01-06 17:42:15 -0800

    add bill and shipping modals

--- App/components/screens/Checkout.js
+++ App/components/screens/Checkout.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import {View, Text, SafeAreaView, TouchableOpacity, Button } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, SafeAreaView, TouchableOpacity, Button, Modal } from 'react-native';
 
 const Checkout = ({navigation, route}) => {
+    const [shipModal, setShipModal] = useState(false);
+    const [billModal, setBillModal] = useState(false);
     
-    return <View>
+    return <SafeAreaView>
         <View>
             {route.params.extra}
         </View>
-    </View>
+        <View>
+            <TouchableOpacity onPress={()=>{
+                setShipModal(true);
+            }}><Text>Shipping information</Text></TouchableOpacity>
+            <TouchableOpacity onPress={()=>{
+                setBillModal(true);
+            }}><Text>Billing information</Text></TouchableOpacity>
+        </View>
+        <Modal visible={shipModal} ><TouchableOpacity style={{width: '100%', height: '100%'}} onPress={()=>{setShipModal(false)}}></TouchableOpacity></Modal>
+        <Modal visible={billModal} ><TouchableOpacity style={{width: '100%', height: '100%'}} onPress={()=>{setBillModal(false)}}></TouchableOpacity></Modal>
+    </SafeAreaView>
 };
 
 export default Checkout;
