3083d6e2f2752e8fe19518cc8233857a2343c1a3
Kevin Gao <kevin.gaobears@berkeley.edu>
2021-01-16 10:55:50 -0800

    fade

--- App/components/FeatherCarousel.js
+++ App/components/FeatherCarousel.js
@@ -1,23 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {View, Text, ScrollView, Button, Animated} from 'react-native';
 import Swiper from 'react-native-deck-swiper';
 
 const data = ['hello', 'world', 'hi', 'DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY'];
 var globalIndex = 0;
+//var previousIndex = 0;
 
 const FeatherCarousel = () => {
+    const swiper = useRef();
     return <Swiper
+    ref = {swiper}
+    goBackToPreviousCardOnSwipeTop={true}
     cards={data}
+    animateCardOpacity={true}
     renderCard={(card) => {
         
         const index = data.indexOf(card);
         console.log('local index', index);
         return <Card index={index} card={card} />;
     }}
-    onSwiped={(cardIndex) => {
-        console.log("current card index", cardIndex);
+    // onSwiped={(cardIndex) => {
+    //     console.log("current card index", cardIndex);
+    //     globalIndex = cardIndex+1;
+    // }}
+    stackSeparation={-20}
+    onSwipedBottom={(cardIndex) => {
+        console.log("swipe bottom", cardIndex);
         globalIndex = cardIndex+1;
     }}
+    onSwipedTop={(cardIndex) => {
+        globalIndex = cardIndex-1;
+        //swiper.swipeBack();
+    }}
+    canSwipeBack={true}
     onSwipedAll={() => {console.log('onSwipedAll')}}
     cardIndex={0}
     backgroundColor={'#4FD0E9'}
@@ -31,18 +46,28 @@ const FeatherCarousel = () => {
 }
 
 const Card = ({index, card}) => {
-    const [fade, setFade] = useState(new Animated.Value(0.8));
+    const [fade, setFade] = useState(new Animated.Value(0.5));
 
     const startAnimation = () => {
         Animated.timing(fade, {
           useNativeDriver: false,
           toValue: 1,
           delay: 0,
-          duration: 300,
+          duration: 200,
+        }).start();
+      };
+
+      const startAnimation2 = () => {
+        Animated.timing(fade, {
+          useNativeDriver: false,
+          toValue: 0.5,
+          delay: 0,
+          duration: 200,
         }).start();
       };
 
       if (index === globalIndex) startAnimation();
+      if (index === globalIndex - 1) startAnimation2();
     return <Animated.View style={{
         height: 500,
         width: 300,
