2cc47138705b22f58ae57c231c2b4c8dd54d8c65
Kevin Gao <kevin.gaobears@berkeley.edu>
2021-01-16 15:25:28 -0800

    checkpoint, parent holds array of ypositions

--- App/components/FeatherPanResponder.js
+++ App/components/FeatherPanResponder.js
@@ -27,7 +27,7 @@ const FeatherPanResponder = ({text, index, getBackPrev, ypositions}) => {
           useNativeDriver: false,
           toValue: 0,
           delay: 0,
-          duration: 700,
+          duration: 300,
         }).start();
       };
 
@@ -48,7 +48,7 @@ const FeatherPanResponder = ({text, index, getBackPrev, ypositions}) => {
         onPanResponderGrant: (event, gesture) => {
         },
         onPanResponderRelease: (event, gesture) => {
-            if (gesture.dy < -200) {
+            if (gesture.dy > 200) {
                 //if (index == data.length-1) return;
                 console.log('swipe up');
                 swipeUpAnimation();
@@ -58,23 +58,78 @@ const FeatherPanResponder = ({text, index, getBackPrev, ypositions}) => {
                     // console.log('resetanimation', yposition);
                 };
                 getBackPrev.push(thisPrev);
+                ypositions.push(yposition);
                 //setDone(true);
                 setIndexState(-100);
-                setYPositionState(-1000- index);
-            } else if (gesture.dy > 200) {
+                setYPositionState(1000 + index);
+            } else if (gesture.dy < -200) {
                 console.log('swipe down', getBackPrev.length);
                 if (getBackPrev.length > 0) {
                     (getBackPrev.pop())();
                     resetAnimation();
 
                 }
+
             } else if ( Math.abs(gesture.dy) < 200) {
                 resetAnimation();
             }
         }
      });
      
+
+
     return <Animated.View style={{justifyContent: 'center', position: 'absolute', top: yposition, zIndex: indexState, borderColor: 'black', borderWidth: 1, backgroundColor: 'yellow', height: '100%', width: '100%'}} {...panResponder.panHandlers} ><Text>{text}</Text></Animated.View>
 }
 
-export default FeatherList;
+const MinimalFeatherParent = () => {
+    const yposition1 = new Animated.ValueXY();
+    const yposition2 = new Animated.ValueXY();
+    return <View style={{position: 'absolute', height: '100%', width: '100%', backgroundColor: 'black'}}>
+        <FeatherChild ypos = {yposition1} ypos2 = {yposition2} index = {1} />
+        <FeatherChild ypos = {yposition2} ypos2 = {yposition1} index = {2} />
+    </View>
+}
+
+const FeatherChild = ({ypos, index, ypos2}) => {
+    const top = index==2;
+    const outofwayAnimation = () => {
+        const newLeft = ypos.getLayout().left>Dimensions.get('window').width?-500:1000;
+        Animated.timing(ypos, {
+          useNativeDriver: false,
+          toValue: {y:1000, x: newLeft},
+          delay: 0,
+          duration: 300,
+        }).start();
+      };
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (event, gesture) => true,
+        onPanResponderMove: (event, gesture) => {
+            if (gesture.dy > 0) {
+            ypos.setValue({ x: gesture.dx, y: gesture.dy });
+            } else if (gesture.dy < 0) {
+                if (!top) {
+                    ypos2.setValue({y: Dimensions.get('window').height + gesture.dy, x: 1000});
+                }
+            }
+        
+        },
+        onPanResponderRelease: (event, gesture) => {
+            if (gesture.dy > 0) {
+                outofwayAnimation();
+            } else if (gesture.dy < 0) {
+                if (!top) {
+                    Animated.timing(ypos2, {
+                        useNativeDriver: false,
+                        toValue: {y:0, x: 0},
+                        delay: 0,
+                        duration: 300,
+                      }).start();
+                }
+            }
+        }
+
+     });
+     return <Animated.View style={{justifyContent: 'center', position: 'absolute', top: ypos.getLayout().top, left: ypos.getLayout().left, borderColor: 'black', borderWidth: 1, backgroundColor: top?'yellow':'green', height: '100%', width: '100%'}} {...panResponder.panHandlers} ></Animated.View>;
+}
+
+export default MinimalFeatherParent;
