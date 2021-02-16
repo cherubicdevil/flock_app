e7b9c4a68cd62db071c5c98de68821ff2827ed5e
Kevin Gao <kevinyanggao@gmail.com>
2021-02-16 14:28:15 -0800

    Fix bugs

--- App/components/screens/Checkout.js
+++ App/components/screens/Checkout.js
@@ -89,7 +89,7 @@ const Checkout = ({navigation, route}) => {
         product: route.params.product,
         type: 'request',
         userId: au.currentUser.uid,
-        members: route.params.data.members,
+        members: route.params.data.memberIds,
     }
     fetch(constants.CHARGE_CUSTOMER_POST, {
         method: 'POST',
