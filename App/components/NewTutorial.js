4bca0dab8cfa0fffebd4ea6d86af80b055f81fa1
Kevin Gao <kevinyanggao@gmail.com>
2021-03-02 11:18:21 -0800

    fix bugs

--- App/components/NewTutorial.js
+++ App/components/NewTutorial.js
@@ -4,11 +4,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { RGBADepthPacking } from 'three';
 import {Portal} from 'react-native-paper';
 import {useDispatch} from 'react-redux';
-import {useFocusEffect} from '@react-navigation/native';
 
 const NewTutorial = ({children, screenId, absolutePosition={left: 0, top: 0, right: 0, bottom: 0}}) => {
   const dispatch = useDispatch();
-    const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(true);
     const [showGif, setShowGif] = useState(false);
 
     const storeData = async () => {
@@ -30,19 +29,13 @@ const NewTutorial = ({children, screenId, absolutePosition={left: 0, top: 0, rig
       }
     }
 
-    useFocusEffect(()=>{
+    useEffect(()=>{
         let used = false;
         getData().then((data)=>{
-            // console.log(data, "new player?");
+            console.log(data, "new player?");
             if (!used) {
-                if (data === null) {
-                  setVisible(true);
-                  setShowGif(true);
-                  setTimeout(()=>{
-                    setShowGif(false);
-                  }, 1200);
-                } else {
-                  setVisible(false);
+                if (data !== null) {
+                setVisible(!data==='true');
                 }
             }
         })
@@ -54,16 +47,11 @@ const NewTutorial = ({children, screenId, absolutePosition={left: 0, top: 0, rig
 
     return <View style={{position: 'absolute', zIndex: 3000, top: 0, height: visible?'100%':0, width: visible?'100%':0, backgroundColor: 'rgba(0,0,0,0.8)', overflow:'hidden'}}>
         <TouchableOpacity style={{height: '100%', width: '100%'}} onPress={()=>{
-
-
-            // setShowGif(true);
+            storeData();
+            setShowGif(true);
             setTimeout(()=>{
               setVisible(false);
-              // setShowGif(false);
-              // console.log('showGif', showGif);
-              storeData();
             },1200);
-
             dispatch({type:'getEggs', payload: 10});
 
 
@@ -74,12 +62,7 @@ const NewTutorial = ({children, screenId, absolutePosition={left: 0, top: 0, rig
 
         </TouchableOpacity>
         <Portal>
-          <TouchableOpacity onPress={()=>{
-            setVisible(false);
-            storeData();
-          }}>
           <Image source={require('App/Assets/Images/egg-anim-proto.gif')} style={{width:visible && showGif?'120%':0, position: 'absolute', bottom: 30, right: "-32%"}} />
-          </TouchableOpacity>
         </Portal>
     </View>
 }
