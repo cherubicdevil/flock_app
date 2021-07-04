a276731c645b9e1f4a97c24b631021f2bfab8f4c
Kevin Gao <kevinyanggao@gmail.com>
2021-07-03 17:36:13 -0700

    fix billing terms

--- App/components/screens/BillingTerms.js
+++ App/components/screens/BillingTerms.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import {View, TouchableOpacity, Button, Text} from 'react-native';
+import {View, TouchableOpacity, Button, Text, Linking, Image} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const BillingTerms = ({navigation}) => {
-    return <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', marginHorizontal: '5%'}}>
-        <Text>Flock uses Stripe to link your credit card</Text>
+    return <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', marginHorizontal: '10%'}}>
+        <View style={{flex: 2, justifyContent: 'center', alignItems: 'center'}}>
+            <Image source= {require('App/Assets/Images/stripe_lock.png')} style={{flex: 1, maxHeight: '80%', alignSelf: 'flex-start', resizeMode: 'contain'}} />
+        </View>
+        <View style={{flex: 3}}>
+        <Text style={{fontSize: 24, textAlign:'center'}}>Flock uses <Text style={{fontWeight: 'bold'}}>Stripe</Text> to link your credit card</Text>
 
-        <Text>Secure</Text>
-        <Text>Encryption helps protect your personal financial data</Text>
+        <View style={{marginTop: 20, marginHorizontal: '0%'}}>
+        
+        <View>
+        <View style={{flexDirection: 'row', alignItems:'flex-start'}}>
+            <Icon name="check-circle" size={20} />
+        <View>
+    
+        <Text style={{marginLeft: 5, fontWeight: 'bold'}}>Secure</Text>
+        <Text style={{marginLeft: 5}}>Encryption helps protect your personal financial data</Text>
+        </View></View>
+        </View>
 
-        <Text>Private</Text>
-        <Text>Your credentials will never be made accessible to Flock</Text>
-        <View style={{position: 'absolute', bottom: '20%'}}>
+        <View style={{marginTop: 15}}>
+        <View style={{flexDirection: 'row', alignItems:'flex-start'}}>
+            <Icon name="check-circle" size={20} />
+        <View>
+    
+        <Text style={{marginLeft: 5, fontWeight: 'bold'}}>Private</Text>
+        <Text style={{marginLeft: 5}}>Your credentials will never be made acessible to Flock</Text>
+        </View></View>
+        </View>
+        </View>
+        </View>
+        
+        <View style={{flex: 1}}>
 
-            <Text style={{fontSize: 10}}>
-                By pressing "Continue", you agree to the <Text>Stripe End User Privacy Policy</Text>
+            <Text style={{fontSize: 12, textAlign: 'center'}}>
+                By pressing "Continue", you agree to the <TouchableOpacity onPress={()=>{
+                    Linking.openURL("https://stripe.com/privacy")
+                }}><Text style={{fontSize: 12, textDecorationLine: 'underline',}}>Stripe End User Privacy Policy</Text></TouchableOpacity>
             </Text>
         <TouchableOpacity style={{marginTop: 30}} title="click me" onPress={()=>{
         navigation.goBack();
     }} >
-        <Text>Continue</Text>
+        <View style={{backgroundColor: 'black', padding: '5%', borderRadius: 5, alignItems: 'center'}}>
+        <Text style={{color: 'white'}}>Continue</Text>
+        </View>
         </TouchableOpacity>
         </View>
         </View>
